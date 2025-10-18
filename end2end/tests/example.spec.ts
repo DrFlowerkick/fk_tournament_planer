380126cd572cb23b89478fd3ca45695bcff493d2
DrFlowerkick <flowerkick14@gmail.com>
2025-10-18 21:55:28 +0200

    ci: :green_heart: Fixing ci to include postgres db.

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from "@playwright/test";
 test("homepage has title and links to intro page", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  await expect(page).toHaveTitle("Welcome to Leptos");
+  await expect(page).toHaveTitle("Welcome to FK Tournament Planer");
 
-  await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
+  await expect(page.locator("h1")).toHaveText("Welcome to FK Tournament Planer!");
 
   await expect(page.getByRole('button')).toHaveText("Click Me: 0");
 
